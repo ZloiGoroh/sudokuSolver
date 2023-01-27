a3121fda7eafb0a824a07b6808291fdc743b0037
ZloiGoroh <cha4anov@gmail.com>
2023-01-27 14:46:59 +0400

    Добавил интерфейс для работы с рядами и подячейками

--- src/store/sudokuStore/getters.ts
+++ src/store/sudokuStore/getters.ts
@@ -18,6 +18,7 @@ const getters: GetterTree<ISudokuState, ISudokuState> = {
 		return (x: number, y: number) =>
 			state.cellsMap.get(getCellCoords(x, y));
 	},
+	// Массив из строк
 	allItems(state, getters) {
 		let rows: SudokuCell[][] = [];
 		getters.maxCountIterate((i: number) => {
@@ -29,6 +30,45 @@ const getters: GetterTree<ISudokuState, ISudokuState> = {
 		});
 
 		return rows;
+	},
+	// Массив из колонн
+	allColumns(state, getters) {
+		let columns: SudokuCell[][] = [];
+		getters.maxCountIterate((i: number) => {
+			let cells: SudokuCell[] = [];
+			getters.maxCountIterate((j: number) => {
+				cells.push(getters.getCellByCoords(i, j));
+			});
+			columns.push(cells);
+		});
+
+		return columns;
+	},
+	allSubGrids(state, getters) {
+		let subGrids: SudokuCell[][][][] = [];
+		// Колонка ряд подблоков
+		getters.maxCountIterate((subGridCounter: number) => {
+			let subGridCol = subGridCounter % state.subgridSize;
+			let subGridRow = Math.floor(subGridCounter / state.subgridSize);
+			if (!subGrids[subGridCol]) {
+				subGrids[subGridCol] = [];
+			}
+			subGrids[subGridCol][subGridRow] = [];
+			// колонка ряд элементов подблока
+			getters.maxCountIterate((cellCounter: number) => {
+				let cellCol = cellCounter % state.subgridSize;
+				let cellRow = Math.floor(cellCounter / state.subgridSize);
+				if (!subGrids[subGridCol][subGridRow][cellCol]) {
+					subGrids[subGridCol][subGridRow][cellCol] = [];
+				}
+				subGrids[subGridCol][subGridRow][cellCol][cellRow] =
+					getters.getCellByCoords(
+						state.subgridSize * subGridCol + cellCol,
+						state.subgridSize * subGridRow + cellRow
+					);
+			});
+		});
+		return subGrids;
 	},
 };
 
